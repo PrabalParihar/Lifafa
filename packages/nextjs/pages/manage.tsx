12179f473f08622a94d9c924ca740c2cae9d24f7
Prabal <prabalparihar95@gmail.com>
2023-10-21 09:01:06 +0530

    manage card

--- packages/nextjs/pages/manage.tsx
+++ packages/nextjs/pages/manage.tsx
@@ -1,15 +1,16 @@
 import type { NextPage } from "next";
 import { ContractReadMethods } from "~~/components/scaffold-eth/Contract/ContractReadMethods";
+import { useContractRead } from "wagmi";
+import {abi} from "../RedEnvelope.json";
 const Manage: NextPage = () => {
-  //   const { data: myEnvelopesData } = ContractReadMethods({
-  //     contractName: "RedEnvelope",
-  //     functionName: "getMyEnvelopes",
-  //     args: [], // If your function doesn't accept any arguments
-  //   });
 
-  //   const myEnvelopesArray = myEnvelopesData.map(value => parseInt(value));
+const { data, isError, isLoading } = useContractRead({
+  address: '0xecb504d39723b0be0e3a9aa33d646642d1051ee1',
+  abi: abi,
+  functionName: 'getMyEnvelopes',
+})
 
-  // return myEnvelopesArray;
+
   
   return <>Manage</>;   
 };
